06b87a0f3e244591d4df15035b34bdbcbfeb6be1
Layfyy <lucasalves@sapo.pt>
2024-05-08 16:56:32 +0100

    Player is appearing and started animations and move logic

--- ShadowsOfThePast/Content/Entidades/Player.cs
+++ ShadowsOfThePast/Content/Entidades/Player.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Xna.Framework.Input;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,56 +11,93 @@ namespace ShadowsOfThePast
 {
     public class Player
     {
+        // HP and MP meter so we know how much hits can the player take and how much spells can he cast
+        public int healthPoints;
+        public int manaPoints;
+        // We need to know the player's score to open the portal eventually
+        public int score;
+        // We need to know if the player is alive or not so we can end the game
+        public bool isAlive;
+
+        // Player sprite and position
+        public Texture2D Texture { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        // Animation variables
+        private int currentFrame;
+        private int totalFrames;
+
+        // Slow down frame animation
+        private int timeSinceLastFrame = 0;
+        private int millisecondsPerFrame = 400;
 
         // Player constructor
-        public Player()
+        public Player(Texture2D texture, int rows, int columns)
         {
-<<<<<<< Updated upstream
-            // HP and MP meter so we know how much hits can the player take and how much spells can he cast
-            int healthPoints = 3;
-            int manaPoints = 10;
-            // We need to know the player's position in the map to draw it in the right place on different levels
-            int startingPosition;
-            // We need to know the player's score to open the portal eventually
-            int score;
-            // We need to know if the player is alive or not so we can end the game
-            bool isAlive;
-=======
             healthPoints = 3;
             manaPoints = 10;
             isAlive = true;
->>>>>>> Stashed changes
+
+            Texture = texture;
+            Rows = rows;
+            Columns = columns;
+            currentFrame = 0;
+            totalFrames = Rows * Columns;
         }
 
 
-        // Player movement method
-        public void Move()
+        // To update the player's
+        public void Update(GameTime gameTime)
         {
-
-            // Check if the player should move right
-            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            timeSinceLastFrame += gameTime.ElapsedGameTime.Milliseconds;
+            if (timeSinceLastFrame > millisecondsPerFrame)
             {
-                // Move the player right
+                timeSinceLastFrame -= millisecondsPerFrame;
 
+                KeyboardState keystate = Keyboard.GetState();
 
-            }
+                // Idle animation
+                if (keystate.GetPressedKeys().Length == 0)
+                    currentFrame++;
+                timeSinceLastFrame = 0;
+                if (currentFrame == 2)
+                    currentFrame = 0;
 
-            // Check if the player should move left
-            if (Keyboard.GetState().IsKeyDown(Keys.A))
-            {
-                // Move the player left
+                // Walking Left Animation
+                if (keystate.IsKeyDown(Keys.D))
+                {
 
+                }
 
-            }
+                // Walking Right Animation
+                if (keystate.IsKeyDown(Keys.A))
+                {
 
-            // Check if the player should jump
-            if (Keyboard.GetState().IsKeyDown(Keys.W))
-            {
-                // Move the player Up
+                }
 
+                // Jumping Animation
+                if (keystate.IsKeyDown(Keys.W))
+                {
 
+                }
             }
         }
+
+        // To draw the player
+        public void Draw(SpriteBatch spriteBatch, Vector2 location, Color color)
+        {
+            int width = 64;
+            int height = 64;
+            int row = (int)((float)currentFrame / Columns);
+            int column = currentFrame % Columns;
+
+            Rectangle sourceRectangle = new Rectangle(width * column, height * row, width, height);
+            Rectangle destinationRectangle = new Rectangle((int)location.X, (int)location.Y, width, height);
+
+            spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, color);
+        }
+
 
 
         // Player fire spell method
@@ -91,14 +130,15 @@ namespace ShadowsOfThePast
         public void TakeDamage(Player player)
         {
             // Deal damage to the player
-            player.healhPoints -= 1;
+            player.healthPoints -= 1;
 
             // Check if the player should die
-            if (healthPoints <= 0)
+            if (player.healthPoints <= 0)
             {
                 // Player is dead
-                isAlive = false;
+                player.isAlive = false;
             }
         }
+
     }
 }
--- ShadowsOfThePast/Game1.cs
+++ ShadowsOfThePast/Game1.cs
@@ -1,60 +1,56 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.IO;
 
-namespace ShadowsOfThePast
+namespace ShadowsOfThePast;
+
+public class Game1 : Game
 {
-    public class Game1 : Game
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+
+    private Dictionary<Vector2, int> background;
+    private Dictionary<Vector2, int> platforms;
+    private Dictionary<Vector2, int> props;
+    private Dictionary<Vector2, int> house;
+    private Dictionary<Vector2, int> collisions;
+    private Texture2D textureDic;
+    private Texture2D collidortext;
+    private Texture2D playertext;
+    private Vector2 camera;
+    private Player player;
+    private SpriteFont font;
+    //const float gravity = 9.8f;
+    private int counter = 0;
+    int score = 0;
+    int display_tilesize = 32;
+
+    public Game1()
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
 
-        public Game1()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-        }
+        //loads all the csv maps from tiled into their respective dictionaries
+        background = LoadMap("../../Data/level1_background.csv");
+        platforms = LoadMap("../../Data/level1_platforms.csv");
+        props = LoadMap("../../Data/level1_props.csv");
+        house = LoadMap("../../Data/level1_house.csv");
+        collisions = LoadMap("../../Data/level1_collisions.csv");
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
-            Player player = new Player();
-
-            base.Initialize();
-        }
-
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
-
-            // TODO: Add your update logic here
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
-            base.Draw(gameTime);
-        }
+        //initializes camera position to zero
+        camera = Vector2.Zero;
     }
 
+    //loads a csv map from tiled and returns a dictionary with the tile positions and tile ids
+    //an csv file is basically a file composed by numbers, the numbers symbolize the tile id, or, the tile used from the mmm png
+    //this dictionary will associate a vector2(position) to an int value(tile id)
     private Dictionary<Vector2, int> LoadMap(string filepath)
     {
+        //creates the dictionary to store the map 
         Dictionary<Vector2, int> result = new();
         StreamReader reader = new(filepath);
 
@@ -62,13 +58,16 @@ namespace ShadowsOfThePast
         string line;
         while ((line = reader.ReadLine()) != null)
         {
+            //reads the csv file and uses a comma as the delimiter for the tile id
             string[] items = line.Split(',');
             for (int x = 0; x < items.Length; x++)
             {
+                //parses string to int
                 if (int.TryParse(items[x], out int value))
                 {
                     if (value > -1)
                     {
+                        //add the tile position and id to the dictionary(tile position as key and id as value)
                         result[new Vector2(x, y)] = value;
                     }
                 }
@@ -80,6 +79,9 @@ namespace ShadowsOfThePast
 
     public bool Victory() //level switcher based on score
     {
+        //if score reaches 500 or more then the score is once again set to 0
+        //and the counter is increased by 1
+        //(condition in draw function makes it so it will only draw level 1 while the counter is 0 and so on)
         if (score >= 500)
         {
             counter++;
@@ -91,9 +93,8 @@ namespace ShadowsOfThePast
     protected override void Initialize()
     {
         // TODO: Add your initialization logic here
-        base.Initialize();
-        Player player = new Player();
 
+        base.Initialize();
     }
 
     protected override void LoadContent()
@@ -102,9 +103,12 @@ namespace ShadowsOfThePast
         //load the tileset png used to make the tileset on tiled
         textureDic = Content.Load<Texture2D>("mmm");
         collidortext = Content.Load<Texture2D>("collision");
+        //loads a font to use on the "score"
         font = Content.Load<SpriteFont>("File");
         playertext = Content.Load<Texture2D>("playertext");
 
+        Texture2D texture = Content.Load<Texture2D>("playertext");
+        player = new Player(texture, 2, 4);
         // TODO: use this.Content to load your game content here
     }
 
@@ -131,6 +135,8 @@ namespace ShadowsOfThePast
             camera.Y += 5;
         }
 
+        player.Update(gameTime);
+
         /*
         int tileWidth = 32;
         int tileHeight = 32;
@@ -152,6 +158,7 @@ namespace ShadowsOfThePast
             player.Velocity.Y += gravity * gameTime.ElapsedGameTime.TotalSeconds;
         }
         */
+
         // TODO: Add your update logic here
 
         base.Update(gameTime);
@@ -166,12 +173,13 @@ namespace ShadowsOfThePast
         _spriteBatch.Begin();
 
         int num_tiles_per_row_png = 14; //number of tiles in a row in the texturedic png
-        int tilesize = 32;
+        int tilesize = 32; //size of tile
 
         if (counter == 0)
         {
-            foreach (var item in background)
+            foreach (var item in background) //for each tile in the background dictionary
             {
+                //caculates the destination rectangle aka where the tile will be drawn on the screen, based on the .key which is the x and y position in the dictionary
                 Rectangle drect = new(
                     (int)item.Key.X * display_tilesize + (int)camera.X, (int)item.Key.Y * display_tilesize + (int)camera.Y, display_tilesize, display_tilesize
                  );
@@ -179,11 +187,15 @@ namespace ShadowsOfThePast
                 int x = item.Value % num_tiles_per_row_png;
                 int y = item.Value / num_tiles_per_row_png;
 
+                //calculates the source rectangle position on the tileset atlas (tilesetDic) based on the tile id given by .value
                 Rectangle src = new(
                     x * tilesize, y * tilesize, tilesize, tilesize
                     );
+                //draws the actual tile on screen until ther are not more items in the choosen dictionary
                 _spriteBatch.Draw(textureDic, drect, src, Color.White);
             }
+
+            //every single foreach below follows the same rules as the one above
 
             foreach (var item in platforms)
             {
@@ -229,6 +241,9 @@ namespace ShadowsOfThePast
                     );
                 _spriteBatch.Draw(textureDic, drect, src, Color.White);
             }
+
+            player.Draw(_spriteBatch, new Vector2(200, 200), Color.White);
+
         }
         else
         {
